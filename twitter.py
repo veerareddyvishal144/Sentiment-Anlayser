d97b8fe7dff562dfe2a6db1ec1a2b352cb17fb30
vishalveerareddy <veerareddyvishal144@gmail.com>
2019-07-18 14:37:53 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,10 +1,10 @@
 import tweepy
 from textblob import TextBlob
 import unicodedata
-cons_key='ekjtv5tJ0EsfdJD6KNMULxJ5N'
-cons_secret='T8xHeMBXz6sKYq0roIPnkAsSGRKJrodR05dPlJMNpaJe49CVaM'
-access_token='2551720986-ycC6dx2ivoKizrvHgugHNUmMAfmNs82ZL5GobPm'
-access_token_secret='E95yv8Xgn0wcl7qhqRbOUxr8EwEntIifiRIuqEjUeV9rH'
+cons_key=''
+cons_secret=''
+access_token=''
+access_token_secret='H'
 auth=tweepy.OAuthHandler(cons_key,cons_secret)
 auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth)
